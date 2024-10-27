6ea4cbf71c2713ea51698d41da880649b0f77628
OrkunDemir5 <orkun.d1999@gmail.com>
2024-10-28 00:00:34 +0300

    Pratik-Inheritance

---
+++ Pratik-Inheritance/Pratik-Inheritance/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratik_Inheritance
+{
+    class BaseKisi
+    {
+        // Properties 
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+
+        // kişi bilgisi yazdırma metodu
+        public void KisiBilgisiYazdir()
+        {
+            Console.WriteLine($"Ad: {Ad}, Soyad: {Soyad}");
+        }
+    }
+
+    // "BaseKisi" sınıfından miras alan Ogrenci sınıfı
+    class Ogrenci : BaseKisi
+    {
+        // Ogrenci sınıfına özgü property
+        public int OgrenciNo { get; set; }
+
+        // Ogrenci bilgilerini yazdıran metot
+        public void OgrenciBilgisiYazdir()
+        {
+            Console.WriteLine($"Öğrenci No: {OgrenciNo}");
+            // KisiBilgisiYazdir metodu, miras alınan sınıftan çağrılıyor
+            KisiBilgisiYazdir();
+        }
+    }
+
+    // "BaseKisi" sınıfından miras alan Ogretmen sınıfı
+    class Ogretmen : BaseKisi
+    {
+        // Ogretmen sınıfına özgü property
+        public decimal Maas { get; set; }
+
+        // Ogretmen bilgilerini yazdıran metot
+        public void OgretmenBilgisiYazdir()
+        {
+            Console.WriteLine($"Maaş: {Maas} TL");
+            // KisiBilgisiYazdir metodu, miras alınan sınıftan çağrılıyor
+            KisiBilgisiYazdir();
+        }
+    }
+}
---
+++ Pratik-Inheritance/Pratik-Inheritance/Program.cs
@@ -0,0 +1,16 @@
+﻿using Pratik_Inheritance;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Örnek bir öğrenci nesnesi oluşturulup, değerler atanır
+        Ogrenci ogrenci = new Ogrenci{Ad = "Orkun",Soyad = "Demir",OgrenciNo = 12345};
+        ogrenci.OgrenciBilgisiYazdir();
+        Console.WriteLine();
+
+        // Örnek bir öğretmen nesnesi oluşturulup, değerler atanır
+        Ogretmen ogretmen = new Ogretmen{Ad = "Yılmaz",Soyad = "Kara",Maas = 7500};
+        ogretmen.OgretmenBilgisiYazdir();
+    }
+}
